c18a5c69f7db373877763944f298d9cd10f91a77
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2014-06-30 15:53:39 +0200

    Scalameter

--- src/main/scala/ChainBenchmark.scala
+++ src/main/scala/ChainBenchmark.scala
@@ -1,17 +1,20 @@
+import org.scalameter.Parameters
 import org.scalameter.api._
-import parsers.truffle.{ParserState, Tests, UninitializedNonterminalCall}
+import parsers.truffle.{NonterminalName, ParserState, Tests, UninitializedNonterminalCall}
+
+import scala.collection.mutable
 
 object ChainBenchmark
 extends PerformanceTest {
 
-  val sizes = Gen.enumeration("chainLength")(50, 100, 150)
+  val sizes = biggerWarmupset(Gen.enumeration("chainLength")(50, 100, 150))
 
-  val unoptimizedParsers = sizes map (chainLength => {
-    UninitializedNonterminalCall.callNodeType = UninitializedNonterminalCall.CallNodeType.UNOPTIMIZED
-    val parser = new ParserState(Tests.repeat('a', 150));
-    val startSymbol = Tests.createChainedProductions(parser, chainLength)
-    (parser, startSymbol)
-  })
+
+  def biggerWarmupset[T](t: Gen[T]): Gen[T] = new Gen[T] {
+    def warmupset = dataset map generate
+    def dataset = t.dataset
+    def generate(params: Parameters) = t.generate(params)
+  }
 
   performance of "Long chains" config (
     // Just want to run one VM, but the Graal-enabled one with custom flags.
@@ -19,10 +22,29 @@ extends PerformanceTest {
     exec.jvmcmd -> "/home/stefan/opt/graalvm-jdk1.8.0-0.3/bin/java",
     exec.jvmflags -> "-server -Xss32m -Dtruffle.TraceRewrites=true -Dtruffle.DetailedRewriteReasons=true -G:+TraceTruffleCompilationDetails -G:+TraceTruffleCompilation -G:TruffleCompilationThreshold=1 -XX:+UnlockDiagnosticVMOptions -XX:CompileCommand=print,*::executeHelper"
     ) in {
+    val parsers: mutable.Map[Int, (ParserState, NonterminalName)] = mutable.HashMap()
+
     performance of "unoptimized" in {
-      using(unoptimizedParsers) in {
-        unoptimizedParser => {
-          val (parser, startSymbol) = unoptimizedParser
+      using(sizes) setUp {
+        chainLength => {
+          parsers getOrElseUpdate(chainLength, {
+            UninitializedNonterminalCall.callNodeType = UninitializedNonterminalCall.CallNodeType.UNOPTIMIZED
+            val parser = new ParserState(Tests.repeat('a', 150))
+            val startSymbol = Tests.createChainedProductions(parser, chainLength)
+
+            println(s"start our warmup loop for ${chainLength}")
+            for (i <- 1 to 10000) {
+              parser.resetParserState()
+              parser.parse(startSymbol)
+            }
+            println(s"finished our warmup loop for ${chainLength}")
+
+            (parser, startSymbol)
+          })
+        }
+      } in {
+        chainLength => {
+          val (parser, startSymbol) = parsers(chainLength)
           parser.resetParserState()
           parser.parse(startSymbol)
         }
